e0d53699326b1e7d7d415543c9a1b83150630cbf
Jay Anslow <jay@anslow.me.uk>
2014-03-31 16:23:51 +0100

    Added JsonController to handle Serializing/Deserializing JSON Strings and Files

---
+++ src/com/jayanslow/projection/world/controllers/JsonController.java
@@ -0,0 +1,80 @@
+package com.jayanslow.projection.world.controllers;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
+import com.jayanslow.projection.world.json.AxisAngle4fSerializer;
+import com.jayanslow.projection.world.json.CuboidScreenSerializer;
+import com.jayanslow.projection.world.json.FlatScreenSerializer;
+import com.jayanslow.projection.world.json.MapSerializerFactory;
+import com.jayanslow.projection.world.json.ProjectorSerializer;
+import com.jayanslow.projection.world.json.RealObjectSerializer;
+import com.jayanslow.projection.world.json.ScreenSerializer;
+import com.jayanslow.projection.world.json.Serializer;
+import com.jayanslow.projection.world.json.SerializerFactory;
+import com.jayanslow.projection.world.json.UniverseSerializer;
+import com.jayanslow.projection.world.json.Vector2fSerializer;
+import com.jayanslow.projection.world.json.Vector3fSerializer;
+import com.jayanslow.projection.world.models.Universe;
+
+public class JsonController {
+	private static Charset			DEFAULT_CHARSET	= StandardCharsets.UTF_8;
+	private final SerializerFactory	f;
+	private Charset					charset;
+
+	public JsonController() {
+		this(DEFAULT_CHARSET);
+	}
+
+	public JsonController(Charset charset) {
+		this.charset = charset;
+
+		f = new MapSerializerFactory(new HashMap<Class<?>, Serializer<?>>());
+
+		// Vecmath Serializers
+		f.addSerializer(new Vector2fSerializer(f));
+		f.addSerializer(new Vector3fSerializer(f));
+		f.addSerializer(new AxisAngle4fSerializer(f));
+
+		f.addSerializer(new RealObjectSerializer(f));
+
+		// RealObject serializers
+		f.addSerializer(new ProjectorSerializer(f));
+		f.addSerializer(new ScreenSerializer(f));
+		f.addSerializer(new UniverseSerializer(f));
+
+		// Screen Serializers
+		f.addSerializer(new CuboidScreenSerializer(f));
+		f.addSerializer(new FlatScreenSerializer(f));
+
+	}
+
+	public Universe deserialize(String json) {
+		JSONObject o = new JSONObject(json);
+		return f.deserialize(Universe.class, o);
+	}
+
+	public String serialize(Universe universe, boolean pretty) {
+		JSONObject o = f.serialize(Universe.class, universe);
+		return o.toString(pretty ? 4 : 0);
+	}
+
+	public Universe readFromFile(String path) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		String json = charset.decode(ByteBuffer.wrap(encoded)).toString();
+		return deserialize(json);
+	}
+
+	public void writeToFile(Universe universe, String path, boolean pretty) throws IOException {
+		String json = serialize(universe, pretty);
+		byte[] encoded = charset.encode(json).array();
+		Files.write(Paths.get(path), encoded);
+	}
+}
