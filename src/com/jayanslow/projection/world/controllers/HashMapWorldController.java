607e47243bd52b201911fe2e955a5feb5a94b183
Jay Anslow <jay@anslow.me.uk>
2014-04-20 00:08:47 +0100

    Implemented WorldListener in WorldController and implementations

--- src/com/jayanslow/projection/world/controllers/HashMapWorldController.java
+++ src/com/jayanslow/projection/world/controllers/HashMapWorldController.java
@@ -1,24 +1,61 @@
 package com.jayanslow.projection.world.controllers;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
+import com.jayanslow.projection.world.listeners.AbstractWorldListenable;
+import com.jayanslow.projection.world.listeners.WorldListener;
 import com.jayanslow.projection.world.models.Projector;
 import com.jayanslow.projection.world.models.RealObject;
 import com.jayanslow.projection.world.models.Screen;
 import com.jayanslow.projection.world.models.Universe;
 
-public class HashMapWorldController implements WorldController {
+public class HashMapWorldController extends AbstractWorldListenable implements WorldController {
 
 	private final Universe						universe;
-	private final HashMap<Integer, Projector>	projectors;
-	private final HashMap<Integer, Screen>		screens;
+	private final HashMap<Integer, Projector>	projectors			= new HashMap<Integer, Projector>();
+	private final HashMap<Integer, Screen>		screens				= new HashMap<Integer, Screen>();
+	private final WorldListener					internalListener	= new WorldListener() {
+
+																		@Override
+																		public void worldChanged() {
+																			fireWorldChange();
+																		}
+
+																	};
 
 	public HashMapWorldController(Universe universe) {
 		this.universe = universe;
-		projectors = new HashMap<Integer, Projector>();
-		screens = new HashMap<Integer, Screen>();
+
+		universe.addWorldListener(internalListener);
+
 		refreshWorld();
+	}
+
+	@Override
+	public void add(RealObject o) {
+		universe.add(o);
+
+		switch (o.getType()) {
+		case PROJECTOR:
+			projectors.put(o.getId(), (Projector) o);
+			break;
+		case SCREEN:
+			screens.put(o.getId(), (Screen) o);
+			break;
+		default:
+			break;
+		}
+
+		o.addWorldListener(internalListener);
+		fireWorldChange();
+	}
+
+	@Override
+	public List<RealObject> getObjects() {
+		return new ArrayList<>(universe.getChildren());
 	}
 
 	@Override
@@ -46,11 +83,12 @@ public class HashMapWorldController implements WorldController {
 		return universe;
 	}
 
-	@Override
 	public void refreshWorld() {
 		projectors.clear();
 		screens.clear();
-		for (RealObject o : universe.getChildren())
+
+		for (RealObject o : universe.getChildren()) {
+			o.addWorldListener(internalListener);
 			switch (o.getType()) {
 			case PROJECTOR:
 				projectors.put(o.getId(), (Projector) o);
@@ -60,7 +98,33 @@ public class HashMapWorldController implements WorldController {
 				break;
 			default:
 				break;
-
 			}
+		}
 	}
+
+	@Override
+	public boolean remove(RealObject object) {
+		if (!universe.remove(object))
+			return false;
+
+		object.removeWorldListener(internalListener);
+
+		switch (object.getType()) {
+		case PROJECTOR:
+			if (projectors.remove(object) != null)
+				return true;
+			break;
+		case SCREEN:
+			if (screens.remove(object) != null)
+				return true;
+			break;
+		default:
+			break;
+		}
+
+		fireWorldChange();
+
+		return true;
+	}
+
 }
--- src/com/jayanslow/projection/world/controllers/WorldController.java
+++ src/com/jayanslow/projection/world/controllers/WorldController.java
@@ -1,12 +1,30 @@
 package com.jayanslow.projection.world.controllers;
 
 import java.util.Collection;
+import java.util.List;
 
+import com.jayanslow.projection.world.listeners.WorldListenable;
 import com.jayanslow.projection.world.models.Projector;
+import com.jayanslow.projection.world.models.RealObject;
 import com.jayanslow.projection.world.models.Screen;
 import com.jayanslow.projection.world.models.Universe;
 
-public interface WorldController {
+public interface WorldController extends WorldListenable {
+	/**
+	 * Adds an object to the universe
+	 * 
+	 * @param object
+	 *            Object to add
+	 */
+	public void add(RealObject object);
+
+	/**
+	 * Gets the objects in the universe
+	 * 
+	 * @return
+	 */
+	public List<RealObject> getObjects();
+
 	/**
 	 * Gets a projector in the world with the specified ID
 	 * 
@@ -47,7 +65,11 @@ public interface WorldController {
 	public Universe getUniverse();
 
 	/**
-	 * Refreshes cache of projectors and screens
+	 * Removes an object from the world
+	 * 
+	 * @param object
+	 *            Object to remove
+	 * @return True if object is removed, otherwise false
 	 */
-	public void refreshWorld();
+	public boolean remove(RealObject object);
 }
